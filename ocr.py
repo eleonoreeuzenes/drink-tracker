cbfe70b773240fc400dd4d06a86af94377a36c28
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2026-01-23 10:23:05 +0100

    second test with medium level

--- ocr.py
+++ ocr.py
@@ -2,11 +2,19 @@ import cv2
 import pytesseract
 
 
-# Read image
-easy_text_path = "assets/easy_text.png"
-easy_img = cv2.imread(easy_text_path)
+def image_to_text(input_path):
+   """
+   A function to read text from images.
+   """
+   img = cv2.imread(input_path)
+   text = pytesseract.image_to_string(img)
 
-# Convert to text
-text = pytesseract.image_to_string(easy_img)
-print(text)
+   return text.strip()
 
+
+# Define image path
+medium_text_path = "assets/medium_text.png"
+
+# Extract text
+extracted_text = image_to_text(medium_text_path)
+print(extracted_text)
